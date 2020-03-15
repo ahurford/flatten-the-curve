a89263df385db4e85bc66ff3db4a812e08ec4484
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-15 11:57:38 -0300

    adjust alpha

--- app.R
+++ app.R
@@ -51,9 +51,10 @@ server <- function(input, output) {
 
 
     ggplot(df, aes(x = time)) +
-      geom_area(aes(y = Ix * 100), fill = '#a6cee3') +
-      geom_hline(aes(yintercept = H)) +
-      geom_area(aes(y = I * 100), fill = '#b2df8a', linetype = 'dashed') +
+      geom_area(aes(y = Ix * 100), fill = '#a6cee3', alpha = 0.5) +
+
+      geom_area(aes(y = I * 100), fill = '#b2df8a', alpha = 0.5) +
+      geom_hline(aes(yintercept = H), alpha = 0.4) +
       labs(x = NULL, y = NULL, title = "% of population infected")
 
     g2 <- ggplot(df, aes(x = time)) +
