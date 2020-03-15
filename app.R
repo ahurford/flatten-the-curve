02d06585360a7d89f19c88e2c09174d82e7423ae
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-15 03:20:35 -0300

    reorg, improve tabsetPanel option

--- app.R
+++ app.R
@@ -89,28 +89,24 @@ server <- function(input, output) {
 borderstyle <- 'outline: 1px solid black; outline-offset: -3px; padding: 10px 10px'
 
 ui <- fluidPage(title = "The math behind flatten the curve",
-
-  # App title ----
-  # titlePanel("The math behind flatten the curve"),
-
   fluidRow(
     column(6,
            h1("The math behind flatten the curve"),
-           p("This app was made by XXX, and anyone interested in contributing should contact ahurford-at-mun-dot-ca.")),
+           p("This app was made by XXX."),
+           p("Anyone interested in contributing should contact ahurford-at-mun-dot-ca.")),
     column(6,
              p("Have you heard the remark:"),
-
              p("We'll never know the effect that social distancing has had;
-        we'll never know how many lives were saved?"
+                we'll never know how many lives were saved?"
              ),
-
              p("We can never answer this question with absolute certainty, but we can get some idea using mathematical
         models.
         ")
            )
     ),
   tabsetPanel(
-    tabPanel("Social Distancing",#5, offset = 1, style = borderstyle,
+    tabPanel("Social distancing",#5, offset = 1, style = borderstyle,
+             column(4,
            p("Below we show that the 'flatten the curve' graphic arises from a mathematical model: the SIR equations (link to wikipedia).
         The lines in the 'flatten the curve' graphic are not simply a drawing of an idea;
         rather, they are based on epidemiological characteristics such the duration of infectivity,
@@ -118,21 +114,25 @@ ui <- fluidPage(title = "The math behind flatten the curve",
         "),
 
            # Input: Slider for the number of bins ----
-           sliderInput("m1", "social distancing (0=none ---> 1=complete isolation):",
+           sliderInput("m1", "social distancing (0=none -> 1=complete isolation):",
                        min = 0, max = 1, step = 0.01, value = .2,
-                       width = '100%'),
+                       width = '100%')),
+           column(8,
 
            # Output:
-           plotOutput("SIR")
+           plotOutput("SIR"))
            # Alec do you know how to make a slider note?
     ),
     tabPanel("Characeristics of the disease",#5, style = borderstyle,
+
            # TODO (AH): update this p with "to the left" etc
+           column(4,
              p("Above we showed that the flatten the curve graph arises from a well-established epidemiological
         model. However, the shape of the curves depend characteristics of the disease. Below we let you choose
         the characteristics of the disease.
-
         "),
+             # TODO (AH): where does this go?
+             p("In my CBC St. John's Morning Show talk, I discussed exponential growth"),
 
            # Input: Slider for the number of bins ----
            sliderInput("H", "hospital capacity:", min = 0, max = 1, value = 0.4),
@@ -140,13 +140,12 @@ ui <- fluidPage(title = "The math behind flatten the curve",
            sliderInput("m1", "hygiene improvement factor:", min = 0, max = 1,step=0.01, value = .2),
            sliderInput("c", "contact rate:", min = 0, max = 10,step=0.1, value = 10),
            sliderInput("m2", "social distancing improvement factor:", min = 0, max = 1,step=0.01, value = .2),
-           sliderInput("chi", "case fatality (%):", min = 0, max = 10, step = 0.1, value = 3),
-           # TODO (AH): where does this go?
-             p("In my CBC St. John's Morning Show talk, I discussed exponential growth")
-
+           sliderInput("chi", "case fatality (%):", min = 0, max = 10, step = 0.1, value = 3)),
+           column(8,
+                  # TODO (AH): include second plot here
        )
   )
-)
+))
 
 
 # Alec #3: I would like a layout with: a sidebar and a plot, and then another sidebar and a plot
