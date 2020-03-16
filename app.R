ed031ee4dad4e26c24aead9d697a5128a85ba39d
Amy Hurford <ahurford@mun.ca>
2020-03-15 23:09:20 -0230

    Update app.R

--- app.R
+++ app.R
@@ -15,77 +15,53 @@ SIR <- function(t, y, p) {
     dSx <- -a * c * Sx * Ix
     dIx <- a * c * Sx * Ix - gamma * Ix - v * Ix
     dFx <- v * Ix
-    if (Ix > H / 100) {
-      dHx <- a * c * Sx * Ix
-    } else {
-      dHx <- 0
-    }
-
     dS <- -a * (1 - m1) * c * S * I
     dI <-  a * (1 - m1) * c * S * I - gamma * I - v * I
     dF <- v * I
-    if (I > H / 100) {
-      dH <- a * (1 - m1) * c * S * I
-    } else {
-      dH <- 0
-    }
-    list(c(Sx = dSx, Ix = dIx, Fx = dFx, Hx = dHx, S = dS,
-           I = dI, Fs = dF, HS = dH))
+
+    list(c(Sx = dSx, Ix = dIx, Fx = dFx, S = dS,
+           I = dI, Fs = dF))
   })
 }
-
-# TODO: is there a better name for this function?
-SIR2 <- function(t, y, p) {
-  with(as.list(c(y, p)), {
-    dSx <- -(1 - m1) * (1 - m2) * a * c * Sx * Ix
-    dIx <- (1 - m1) * (1 - m2) * a * c * Sx * Ix - gamma * Ix - v * Ix
-    dS <- -a * c * S * I
-    dI <-  a * c * S * I - gamma * I - v * I
-    list(c(
-      Sx = dSx,
-      Ix = dIx,
-      S = dS,
-      I = dI
-    ))
-  })
-}
-
 
 server <- function(input, output) {
   output$SIR <- renderPlot({
-    # Parameters are taken for Bolker & Dushoff model
-    gamma <- 1
+    # Parameters are taken from Bolker & Dushoff model
+    gamma <- 1/13
     chi <- 0.03
     v <- gamma * chi / (1 - chi)
-    H <- 40
-    c <- 7
-    a <- 1
-    print(a*c/(v+gamma))
+    H <- 15
+    c <-0.4
+    a<-0.5
     parms <- c(a = a, m1 = input$m1, c = c, gamma = gamma,
                v = v, H = H)
-    I0 <- 0.01
+    I0 <- 0.005
     S0 <- 1 - I0
     mintime <- 0
-    maxtime <- 6
-    out <- ode(y = c(Sx = S0, Ix = I0, Fx = 0, Hx = 0, S = S0, I = I0, FS = 0, HS = 0), times = seq(mintime, maxtime, .1), SIR, parms)
+    maxtime <- 150
+    out <- ode(y = c(Sx = S0, Ix = I0, Fx = 0, S = S0, I = I0, FS = 0), times = seq(mintime, maxtime, 1), SIR, parms)
     df <- data.frame(out)
 
     areaAlpha <- 0.6
     g1 <- ggplot(df, aes(x = time)) +
       geom_area(aes(y = Ix * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
       geom_area(aes(y = I * 100), fill = '#b2df8a', alpha = areaAlpha) +
-      geom_hline(aes(yintercept = H), alpha = 0.2, size=3) +
+      geom_hline(aes(yintercept = H), alpha = 0.2, size=3)+
       labs(x = NULL, y = NULL, title = "Percent of population infected")
 
     g2 <- ggplot(df, aes(x = time)) +
       geom_area(aes(y = Fx * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
       geom_area(aes(y = FS * 100), fill = '#b2df8a', alpha = areaAlpha) +
-      labs(x = "time (months)", y = NULL, title = "Cumulative fatalities")
+      labs(x = "time (days)", y = NULL, title = "Cumulative fatalities")
 
     # TODO (AH): update these placeholders
-    R_0 <- 10
-    R_2 <- 12
-    toprint <- data.frame(R_0, R_2)
+    
+    label<-""
+    R_0 <- round(a*c/(v+gamma),2)
+    R_2 <- round((1-input$m1)*R_0,2)
+    DT <- 2
+    DT2 <- 2 
+    toprint <- data.frame("no social distancing"=label,"Doubling time" = DT,"R0"=R_0,"social distancing"=label, "R0"=R_2, "Doubling time" = DT2)
 
 
     (g1 /
@@ -93,37 +69,6 @@ server <- function(input, output) {
       scale_y_continuous(expand = expand_scale(c(0, 0.1)), labels = function(x) paste0(x,"%")) &
         scale_x_continuous(expand = c(0, 0)) ) /
       tableGrob(toprint, rows = NULL, theme = ttheme_minimal())
-  })
-
-  output$more <- renderPlot({
-    gamma = 0.5
-    v <- gamma * input$chi / (1 - input$chi / 100) / 100
-    # TODO: input$m1 is never used
-    parms <- c(a = input$a, m1 = input$m1, c = input$c, m2 = input$m2, gamma = gamma, v = v)
-    # TODO: input$H is never used
-    H <- input$H
-    I0 <- 0.01
-    S0 <- 1 - I0
-    out <- ode(y = c(Sx = S0, Ix = I0, S = S0, I = I0), times = seq(0, 12, .1), SIR2, parms)
-    df <- data.frame(out)
-
-    areaAlpha <- 0.6
-
-
-    g1 <- ggplot(df, aes(x = time)) +
-      geom_area(aes(y = Ix * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
-      geom_area(aes(y = I * 100), fill = '#b2df8a', alpha = areaAlpha) +
-      geom_hline(aes(yintercept = H), alpha = 0.2) +
-      labs(x = NULL, y = NULL, title = "Percent of population infected") + scale_y_continuous(expand = expand_scale(c(0, 0.1)), labels = function(x) paste0(x,"%")) +
-      scale_x_continuous(expand = c(0, 0))
-
-
-    # TODO (AH): update these placeholders
-    R_0 <- a*c*1/(gamma+v)
-    R_2 <- 12
-    toprint <- data.frame(R_0, R_2)
-
-    g1 / tableGrob(toprint, rows = NULL, theme = ttheme_minimal())
   })
 } # End server function
 
@@ -140,11 +85,17 @@ ui <- fluidPage(title = "The math behind flatten the curve",
     ),
   tabsetPanel(
     tabPanel("Social distancing",
-             column(3,
+             column(5,
+                    p(""),
+                    
+                    # Input: Slider for the number of bins ----
+                    sliderInput("m1", "social distancing:",
+                                min = 0, max = 1, step = 0.01, value = .2,
+                                width = '100%'),helpText("0: no efforts to enact social distance"),helpText("1: fully effective isolation"),
                     p(""),
                     p("Have you heard the remark:"),
-                    p("\"We'll never know the effect that social distancing has had;
-                      we'll never know how many lives were saved\""
+                    p(tags$b("We'll never know the effect that social distancing has had;
+                      we'll never know how many lives were saved")
                     ),
                     p("We can never know this with absolute certainty,
                       but we can get some idea using mathematical models. "),
@@ -158,11 +109,8 @@ and today, these equations remain a good start for understanding the time course
 are the computational output due to solving the SIR equation.
         "),
 
-           # Input: Slider for the number of bins ----
-           sliderInput("m1", "social distancing:",
-                       min = 0, max = 1, step = 0.01, value = .2,
-                       width = '100%'),helpText("0: no efforts to enact social distance"),helpText("1: fully effective isolation"), ),
-           column(9,
+),
+           column(7,
 
            # Output:
            plotOutput("SIR"),
