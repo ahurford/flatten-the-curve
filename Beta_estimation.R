4ee5247349b838b4ffb9dd71e8d4a52ddfdfcc26
Amy Hurford <ahurford@mun.ca>
2020-03-18 07:48:21 -0230

    Update Beta_estimation.R
    fit corrected to go off cumulative cases

--- Beta_estimation.R
+++ Beta_estimation.R
@@ -1,6 +1,6 @@
 rm(list=ls()) 
 # Code to be embedded in the app
-# Opps need to fit to cumulative cases
+
 library(deSolve)
 library(bbmle)
 
@@ -9,31 +9,36 @@ SIR.fit <- function(t, y, p) {
     dS <- -beta * S * I
     dI <-  beta * S * I - gamma * I - v * I
     dF <- v * I
-    list(c(S = dS,I = dI, Fs = dF))
+    dC <- beta*S*I
+    list(c(S = dS,I = dI, Fs = dF, C=dC))
   })
 }
 
 nLL = function(beta){
-  
   parms <- c(beta = beta, gamma = 1/13, v = 0.00237)
   I0 <- 0.005
   S0 <- 1 - I0
   mintime <- 0
   maxtime <- 250
-  out <- ode(y = c(S = S0, I = I0, FS = 0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
+  out <- ode(y = c(S = S0, I = I0, FS = 0, C=0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
   df <- data.frame(out)
  
   Model.pred=NULL
-  for(i in seq(1,length(Idata$I))){
+  # Extract the model predictions corresponding to the times
+  # in the data
+  for(i in seq(1,length(Idata$cumI))){
     ii = which(df$time==Idata$time[i])
-    Model.pred[i] = df$I[ii]
+    Model.pred[i] = df$cumI[ii]
   }
-  res = sum((Model.pred - Idata$I)^2)
+  # The negative LL. The RHS has dropped constants
+  # that don't affect the location of the maximum
+  res = sum((Model.pred - Idata$cumI)^2)
   return(res)
 }
 
-# Fake data
-Idata <- data.frame(time=c(1,5,10), I = c(.1,.12,.13))
+# Some data that was created to test the method:
+Idata <- data.frame(time=c(1,5,10), cumI = c(.1,.12,.13))
+# Find the MLE beta
 fit = mle2(nLL, data = Idata, start=list(beta=.4))
 beta.est=unname(coef(fit))
 parms <- c(beta = beta.est, gamma = 1/13, v = 0.00237)
@@ -42,19 +47,32 @@ S0 <- 1 - I0
 mintime <- 0
 maxtime <- 250
 
-out <- ode(y = c(S = S0, I = I0, FS = 0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
+out <- ode(y = c(S = S0, I = I0, FS = 0, C=0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
 df = data.frame(out)
-plot(df$time, df$I, typ="l")
-points(Idata$time, Idata$I)
+
+par(mfrow = c(1,3))
+plot(df$time, df$C, typ="l")
+points(Idata$time, Idata$cumI)
 
 # parameteric 95% CI
 LowerCI = max(beta.est - 1.96*unname(stdEr(fit)),0)
 UpperCI = beta.est + 1.96*unname(stdEr(fit))
+# Lower 95% CI
 parms <- c(beta = LowerCI, gamma = 1/13, v = 0.00237)
-out.lower <- ode(y = c(S = S0, I = I0, FS = 0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
-parms <- c(beta = UpperCI, gamma = 1/13, v = 0.00237)
-out.upper <-out <- ode(y = c(S = S0, I = I0, FS = 0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
+out.lower <- ode(y = c(S = S0, I = I0, FS = 0, C=0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
 df.lower = data.frame(out.lower)
+lines(df.lower$time, df.lower$C, lty=2)
+
+# Upper 95% CI
+parms <- c(beta = UpperCI, gamma = 1/13, v = 0.00237)
+out.upper <-out <- ode(y = c(S = S0, I = I0, FS = 0, C=0), times = seq(mintime, maxtime, 1), SIR.fit, parms)
 df.upper = data.frame(out.upper)
-lines(df.lower$time, df.lower$I, lty=2)
+lines(df.upper$time, df.upper$C, lty=2)
+
+plot(df$time, df$I, typ="l")
 lines(df.upper$time, df.upper$I, lty=2)
+lines(df.lower$time, df.upper$I, lty=2)
+
+# The last plot needs to be of
+# time of the release of the last data vs.
+# the new for this new data beta.est with 95% CIs 
