d0ef5015aa2928b08b877869a6b892efe975885e
Amy Hurford <ahurford@mun.ca>
2020-03-18 21:28:13 -0230

    Update app.R
    new panel progress

--- app.R
+++ app.R
@@ -34,6 +34,32 @@ SIR <- function(t, y, p) {
   })
 }
 
+SIR2 <- function(t, y, p) {
+  with(as.list(c(y, p)), {
+    dSx <- -a * c * Sx * Ix
+    dIx <- a * c * Sx * Ix - gamma * Ix - v * Ix
+    dFx <- v * Ix
+    dCx <- a*c*Sx*Ix
+    if(Ix > H2/100) {
+      dHx <- a*c*Sx*Ix
+      } else {
+      dHx<-0
+      }
+    dS <- -a * (1 - m2) * c * S * I
+    dI <-  a * (1 - m2) * c * S * I - gamma * I - v * I
+    dF <- v * I
+    dC <- a*c*(1-m2)*S*I
+    if(I > H/100) {
+      dH <- (1-m2)*a*c*S*I
+      } else {
+      dH <-0
+    }
+    
+    list(c(Sx = dSx, Ix = dIx, Fx = dFx, Hx = dHx, Cx = dCx, S = dS,
+           I = dI, Fs = dF, C=dC, HS = dH))
+  })
+}
+
 ### Server ----
 server <- function(input, output) {
   output$SIR <- renderPlot({
@@ -59,8 +85,8 @@ server <- function(input, output) {
     # Plot percent population infected
     areaAlpha <- 0.6
     g1 <- ggplot(df, aes(x = time)) +
-      geom_area(aes(y = Cx * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
-      geom_area(aes(y = C * 100), fill = '#b2df8a', alpha = areaAlpha) +
+      geom_area(aes(y = Ix * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
+      geom_area(aes(y = I * 100), fill = '#b2df8a', alpha = areaAlpha) +
       geom_hline(aes(yintercept = H), alpha = 0.2, size = 3) +
       labs(x = NULL, y = NULL, title = "Percent of the population currently infected")
 
@@ -69,14 +95,6 @@ server <- function(input, output) {
       geom_area(aes(y = Fx * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
       geom_area(aes(y = FS * 100), fill = '#b2df8a', alpha = areaAlpha) +
       labs(x = "time (days)", y = NULL, title = "Cumulative fatalities (% of the population)")
-
-    # xTODO (Alec): this is what I want to plot but with ggplot
-    # TODO (Amy): update labs
-    g3 <- ggplot(df, aes(x = time)) +
-      geom_area(aes(y = diffC), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
-      geom_area(aes(y = diffCx), fill = '#b2df8a', alpha = areaAlpha) +
-      # geom_hline(aes(yintercept = H), alpha = 0.2, size = 3) +
-      labs(x = NULL, y = NULL, title = "TODO: C, Cx")
 
     # Generate data.frame to print
     R_0 <- round(a * c / (v + gamma), 1)
@@ -96,12 +114,77 @@ server <- function(input, output) {
 
     # Combine plots and table with patchwork
     (g1 /
-      g2 /
-        g3 &
+      g2 &
         scale_y_continuous(expand = expand_scale(mult = c(0, 0.1)),
                          labels = function(x) paste0(x, "%")) &
         scale_x_continuous(expand = expand_scale(mult = c(0, 0)))) /
       tableGrob(toprint, rows = NULL, theme = ttheme_minimal())
+  })
+  
+  output$AreaUnder <- renderPlot({
+    # Parameters are taken from Bolker & Dushoff model
+    gamma <- 1/13
+    chi <- 0.03
+    v <- gamma * chi / (1 - chi)
+    c <- 0.4
+    a <- 0.5
+    parms <- c(a = a, m2 = input$m2, c = c, gamma = gamma,
+               v = v, H2 = input$H2)
+    I0 <- 0.005
+    S0 <- 1 - I0
+    mintime <- 0
+    maxtime <- 250
+    out <- ode(y = c(Sx = S0, Ix = I0, Fx = 0, Hx=0, Cx=0, S = S0, I = I0, FS = 0, C=0, HS=0), times = seq(mintime, maxtime, 1), SIR2, parms)
+    df <- data.table(out)
+    
+    df[, diffC := c(diff(C), NA)]
+    df[, diffCx := c(diff(Cx), NA)]
+    #df$Hx =  max(df$Hx-H,0)
+    #df$HS = max(df$HS-H,0)
+    print(df$Hx)
+    
+    # Plot percent population infected
+    areaAlpha <- 0.6
+    g1 <- ggplot(df, aes(x = time)) +
+      geom_area(aes(y = Ix * 100), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
+      geom_area(aes(y = I * 100), fill = '#b2df8a', alpha = areaAlpha) +
+      geom_hline(aes(yintercept = input$H2), alpha = 0.2, size = 3) +
+      labs(x = NULL, y = NULL, title = "Percent of the population currently infected")
+
+    g3 <- ggplot(df, aes(x = time)) +
+      geom_area(aes(y = 100*diffCx), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
+      geom_area(aes(y = 100*diffC), fill = '#b2df8a', alpha = areaAlpha) +
+      # geom_hline(aes(yintercept = H), alpha = 0.2, size = 3) +
+      labs(x = "time (days)", title = "Percent of the population newly infected", y= NULL)
+    
+    # g2 <- ggplot(df, aes(x = time)) +
+    #   geom_area(aes(y = 100*Hx), fill = '#a6cee3', alpha = areaAlpha - 0.2) +
+    #   geom_area(aes(y = 100*HS), fill = '#b2df8a', alpha = areaAlpha) +
+    #   # geom_hline(aes(yintercept = H), alpha = 0.2, size = 3) +
+    #   labs(x = "time (days)", title = "Percent of the population newly infected", y= NULL)
+    
+    infect.days.x = round(100*sum(df$Ix),0)
+    infect.days = round(100*sum(df$I),0)
+    num.infect = round(100*sum(diff(df$C)),1)
+    num.infect.x = round(100*sum(diff(df$Cx)),1)
+    
+    toprint <-
+      data.frame(
+        " " = c("no distancing", "with distancing"),
+        "infection x days" = c(infect.days.x, infect.days),
+        "total infected (%)" = c(num.infect.x, num.infect),
+        check.names = FALSE
+      )
+    
+    # Combine plots and table with patchwork
+    (g1 /
+        g3 
+      &
+        scale_y_continuous(expand = expand_scale(mult = c(0, 0.1)),
+                           labels = function(x) paste0(x, "%")) &
+        scale_x_continuous(expand = expand_scale(mult = c(0, 0)))) /
+      tableGrob(toprint, rows = NULL, theme = ttheme_minimal())
+    
   })
 
   output$scrapeTab <- renderTable({
@@ -191,7 +274,50 @@ ui <- fluidPage(title = "The math behind flatten the curve",
                     that, even aside from preventing overwhelming the health care system, a smaller percentage of the population
                     will die from COVID-19 under social distancing."),
            helpText("The parameterization for this SIR model was taken from Bolker and Dushoff (2020)."))),
-
+    # Area under the curve
+    tabPanel("Area under the curve",
+             # Left column
+             tabPanel("Social distancing",
+                      column(5,
+                             p(""),
+                             
+                             # Slider input: social distancing
+                             sliderInput("m2", "social distancing:",
+                                         min = 0, max = 1, step = 0.01, value = .2,
+                                         width = '100%'),
+                             helpText("0: no efforts to enact social distancing"),
+                             helpText("1: fully effective isolation"),
+                             sliderInput("H2", "Hospital capacity:",
+                                         min = 0, max = 100, step = 1, value = 15,
+                                         width = '100%'),
+                             
+                             # Text in sidebar
+                             p("Have you heard the remark:"),
+                             p(tags$b(" 'We'll never know the effect that social distancing has had;
+                                      we'll never know how many lives were saved' ")),
+                             p("While we can never know with certainty,
+                               we can get some idea using epidemic models. 'Flatten the curve'
+                               argues that effective social distancing (green curves) will lessen the maximum number of infected people during an
+                               epidemic, so that hospital resources are not overwhelmed (grey line).
+                               On the right, we show that 'flatten the curve' arises from a mathematical model that describes the dynamics of an
+                               epidemic, specifically ",
+                               tags$a(href = "https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIR_model", "the SIR equations.")),
+                             p("The 'flatten the curve' graphic is not simply a drawing of an idea;
+                               rather it can arise based on disease characteristics and the interactions between
+                               susceptible and infected people. The SIR equations have been used for decades,
+                               and today these equations remain a good start for understanding how the
+                               number of infected people changes over time. The graphs on the right
+                               are the computational output due to solving the SIR equations.
+                               To make these graphs, we needed to define disease
+                               characterisitcs such as the duration of infectivity (assumed to be 13 days),
+                               and the percentage of infections that lead to fatalities (assumed to be 3%)."),
+                             p("Not all the 'flatten the curve' graphs that have appeared in the media arise from SIR or related epidemic models",
+                               tags$a(href = "http://ms.mcmaster.ca/~bolker/misc/peak_I_simple.html", "(Bolker and Dushoff 2020)"), "but none-the-less, as we have shown in the
+                               graphs on the right, the 'flatten the curve' idea is consistent with
+                               the epidemic models commonly found in textbooks.")),
+                      column(7,
+             plotOutput("AreaUnder")
+    ))),
     # More models tab
     tabPanel("More models",
              column(10,
