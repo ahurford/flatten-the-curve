3ba39b1fdfb7a8855027af33ed1abd6cc7c79a96
Amy Hurford <ahurford@mun.ca>
2020-03-31 11:17:43 -0230

    temp update
    temporary update for independent article

--- app.R
+++ app.R
@@ -350,10 +350,10 @@ server <- function(input, output) {
    	lines(df$time, df$C*pop.size/1000, col='#a6cee3',lwd=4)
     points(Days.Since,(NLData$presumptive_positive+NLData$confirmed_positive)/1000, pch = 16)
 
-    plot(tail(df$time, -1), c(diff(df$C)*pop.size), typ="l", ylab = "new cases",las=1, xlab = "days since first case on March 16", lwd=4, col='#b2df8a', xlim = c(0, max(Days.Since)+10),ylim = c(0,35))
+    #plot(tail(df$time, -1), c(diff(df$C)*pop.size), typ="l", ylab = "new cases",las=1, xlab = "days since first case on March 16", lwd=4, col='#b2df8a', xlim = c(0, max(Days.Since)+10),ylim = c(0,35))
   #ylim =c(0,max(diff(df$C)*pop.size, diff(dfnull$C)*pop.size)
-    lines(tail(dfnull$time, -1), c(diff(dfnull$C)*pop.size), col = '#a6cee3',lwd=4)
-    points(Days.Since, NewCasesPerDay, pch=16)
+    #lines(tail(dfnull$time, -1), c(diff(dfnull$C)*pop.size), col = '#a6cee3',lwd=4)
+    plot(Days.Since, NewCasesPerDay, pch=16, xlab = "days since first case on March 16", ylab = "new cases")
     #points(tail(NLData$presumptive_positive+NLData$confirmed_positive, -1) - head(NLData$presumptive_positive+NLData$confirmed_positive, -1))
 
     # t <-seq(11,100, .1)
