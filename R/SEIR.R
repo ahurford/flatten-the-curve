93b4c736e3174567bc4cec3c0921c36a58e792b5
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-03 14:17:30 -0300

    tidy SEIR

--- R/SEIR.R
+++ R/SEIR.R
@@ -1,28 +1,41 @@
 SEIR <- function(t, y, p) {
 	with(as.list(c(y, p)), {
-		if (t <= today){
-			dS <- -beta * S * (I+E)
-			dE <- beta * S * (I+E) - a1*E
-			dI <- a1*E - gamma1 * I - v1 * I
-			dC <- a1*E
-		} else
-		{
-			dS <- -beta1 * S * (I+E)
-			dE <- beta1 * S * (I+E) - a1*E
-			dI <- a1*E - gamma1 * I - v1 * I
-			dC <- a1*E
+
+		if (t <= today) {
+			dS <- -beta * S * (I + E)
+			dE <- beta * S * (I + E) - a1 * E
+			dI <- a1 * E - gamma1 * I - v1 * I
+			dC <- a1 * E
+		} else {
+			dS <- -beta1 * S * (I + E)
+			dE <- beta1 * S * (I + E) - a1 * E
+			dI <- a1 * E - gamma1 * I - v1 * I
+			dC <- a1 * E
 		}
-		list(c(S = dS,E=dE, I = dI, C = dC))
+
+		list(c(
+			S = dS,
+			E = dE,
+			I = dI,
+			C = dC
+		))
 	})
 }
 
 
 SEIRnull <- function(t, y, p) {
 	with(as.list(c(y, p)), {
-		dS <- -beta * S * (I+E)
-		dE <- beta * S * (I+E) - a1*E
-		dI <- a1*E - gamma1 * I - v1 * I
-		dC <- a1*E
-		list(c(S = dS,E=dE, I = dI, C = dC))
+
+		dS <- -beta * S * (I + E)
+		dE <- beta * S * (I + E) - a1 * E
+		dI <- a1 * E - gamma1 * I - v1 * I
+		dC <- a1 * E
+
+		list(c(
+			S = dS,
+			E = dE,
+			I = dI,
+			C = dC
+		))
 	})
 }
